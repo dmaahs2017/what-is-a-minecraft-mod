2c913ce65b4a3055ac1447b6a464b421df8c7fd4
dalton <maahs2017@gmail.com>
2022-05-05 18:50:48 -0500

    setup newblock mod structure

---
+++ src/main/java/net/fabricmc/newblock/BlockMod.java
@@ -0,0 +1,12 @@
+package net.fabricmc.newblock;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlockMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
